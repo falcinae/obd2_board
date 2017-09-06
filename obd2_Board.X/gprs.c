3a00b4c98f89ead25d45091a45f577a2c5eba6c3
Javier Alcina <javier.alcina@ontech.es>
2017-09-06 21:45:56 +0200

    Commit hecho desde Ontech

--- obd2_Board.X/gprs.c
+++ obd2_Board.X/gprs.c
@@ -1,14 +1,64 @@
+/*************************************************************************
+Nombre del fichero: 	gprs.c
+Tipo de fichero: 		Source File
+Fecha de creacion: 		17-Abril-2017
+Ultima modificacion: 	17-Abril-2017
+Compañia:				Universidad de Cádiz
+Responsable: 			Javier Alcina
+ 
+
+Proposito:
+En este fichero se definirán todas las funciones referentes a las 
+comunicaciones por GPRS.
+
+
+Lista de modificaciones:
+
+************************************************************************/
 
 #include "gprs.h" 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+/*************************************************************************
+    Nombre de la función: 	ConfigGprsPort()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Configurar el puerto de comunicaciones con el modem GPRS
+
+    Precondiciones:
+        Ninguna
+
+    Returns
+        Ninguno
+
+    Parametros
+        Ninguno
+************************************************************************/
 void ConfigGprsPort (void)
 {
     UART3_Initialize();
 }
 
+/*************************************************************************
+    Nombre de la función: 	CheckSIM()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para comprobar que la tarjeta SIM está introducida
+
+    Precondiciones:
+        La tarjeta SIM introducida debe tener desactivado el PIN
+
+    Returns
+        0 -> Tarjeta SIM ok
+        1 -> Error en la lectura de SIM
+
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char CheckSIM (void)
 {
     char *bufferToSend;
@@ -27,6 +77,23 @@ unsigned char CheckSIM (void)
 
 }
 
+/*************************************************************************
+    Nombre de la función: 	CheckNetwork()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para comprobar que el modem está conectado a la red
+
+    Precondiciones:
+        La tarjeta SIM introducida debe tener desactivado el PIN
+
+    Returns
+        0 -> Conexión a red OK
+        1 -> Error en conexión a red
+
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char CheckNetwork (void)
 {
     char bufferToSend[50];
@@ -45,24 +112,64 @@ unsigned char CheckNetwork (void)
 
 }
 
-unsigned char CheckSignalQuality (void)
+/*************************************************************************
+    Nombre de la función: 	CheckSignalQuality()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para comprobar que el modem está conectado a la red
+
+    Precondiciones:
+        La tarjeta SIM introducida debe tener desactivado el PIN
+
+    Returns
+        00 -> -113dBm o menos
+        01 -> -111dBm
+        02..30 -> -109..-53 dBm
+        31 -> -51 dBm o mayor
+        99 -> Desconocido
+
+    Parametros
+        Ninguno
+************************************************************************/
+int CheckSignalQuality (void)
 {
+    char singalQualityBuffer[15];
     char bufferToSend[50];
     char *bufferRead;
     
     sprintf(bufferToSend, "%s", AT_COMMAND_CHECK_SIGNAL_QUALITY);
     UART3_WriteBuffer(bufferToSend, strlen(bufferToSend));
-    UART3_ReadBuffer(bufferRead, 4);
+    UART3_ReadBuffer(bufferRead, 10);
     
-    if (strstr(bufferRead, AT_OK_RESPONSE) != NULL)
+    if (strstr(bufferRead, SIGNAL_QUALITY_RESPONSE) != NULL)
     {
-        return 0;  
+        memcpy(singalQualityBuffer,bufferRead[6], 2);
+        singalQualityBuffer[2] = 0;
+        return atoi(singalQualityBuffer);
     }
     else
-        return 1;
-
+        return GENERAL_ERROR;
 }
 
+/*************************************************************************
+    Nombre de la función: 	RetrieveNetworkOperator()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para comprobar que el modem está conectado al operador
+        que corresponde
+
+    Precondiciones:
+        Debe ejectarse un CheckSIM();
+
+    Returns
+        0 -> Ha encontrado la red definida en OPERATOR_NAME
+        1 -> Error en conexión con operador o conexión a otro operador
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char RetrieveNetworkOperator (void)
 {
     char bufferToSend[50];
@@ -70,9 +177,9 @@ unsigned char RetrieveNetworkOperator (void)
     
     sprintf(bufferToSend, "%s", AT_COMMAND_RETRIEVE_OPERATOR);
     UART3_WriteBuffer(bufferToSend, strlen(bufferToSend));
-    UART3_ReadBuffer(bufferRead, 4);
+    UART3_ReadBuffer(bufferRead, 25);
     
-    if (strstr(bufferRead, AT_OK_RESPONSE) != NULL)
+    if (strstr(bufferRead, OPERATOR_NAME) != NULL)
     {
         return 0;  
     }
@@ -81,6 +188,23 @@ unsigned char RetrieveNetworkOperator (void)
 
 }
 
+/*************************************************************************
+    Nombre de la función: 	EstablishGprsConnection()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para establecer conectividad GPRS
+
+    Precondiciones:
+        Debe ejectarse un CheckSIM();
+
+    Returns
+        0 -> Se ha establecido conexión
+        1 -> Error en el establecimiento de conexión
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char EstablishGprsConnection (void)
 {
     char bufferToSend[50];
@@ -99,6 +223,23 @@ unsigned char EstablishGprsConnection (void)
 
 }
 
+/*************************************************************************
+    Nombre de la función: 	ConnectWithOurServer()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para conectar con servidor establecido
+
+    Precondiciones:
+        Debe haber una conexión GPRS activa
+
+    Returns
+        0 -> Se ha establecido conexión
+        1 -> Error en el establecimiento de conexión
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char ConnectWithOurServer (void)
 {
     char bufferToSend[50];
@@ -117,6 +258,23 @@ unsigned char ConnectWithOurServer (void)
 
 }
 
+/*************************************************************************
+    Nombre de la función: 	CheckServerConnection()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para verificar que se ha conectado al servidor
+
+    Precondiciones:
+        Debe haberse una conexión activa 
+
+    Returns
+        0 -> Se ha establecido conexión
+        1 -> Error en el establecimiento de conexión
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char CheckServerConnection (void)
 {
     char bufferToSend[50];
@@ -126,7 +284,7 @@ unsigned char CheckServerConnection (void)
     UART3_WriteBuffer(bufferToSend, strlen(bufferToSend));
     UART3_ReadBuffer(bufferRead, 4);
     
-    if (strstr(bufferRead, AT_OK_RESPONSE) != NULL)
+    if (strstr(bufferRead, SERVER_NAME) != NULL)
     {
         return 0;  
     }
@@ -135,6 +293,23 @@ unsigned char CheckServerConnection (void)
 
 }
 
+/*************************************************************************
+    Nombre de la función: 	OpenGprsSession()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para establecer conectividad GPRS
+
+    Precondiciones:
+ Abre el contexto PDP o sesión 
+
+    Returns
+        0 -> Se ha establecido conexión
+        1 -> Error en el establecimiento de conexión
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char OpenGprsSession (void)
 {
     char bufferToSend[50];
@@ -153,6 +328,23 @@ unsigned char OpenGprsSession (void)
 
 }
 
+/*************************************************************************
+    Nombre de la función: 	CloseGprsSession()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para establecer conectividad GPRS
+
+    Precondiciones:
+        Debe ejectarse un CheckSIM();
+
+    Returns
+        0 -> Se ha establecido conexión
+        1 -> Error en el establecimiento de conexión
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char CloseGprsSession (void)
 {
     char bufferToSend[50];
@@ -171,6 +363,23 @@ unsigned char CloseGprsSession (void)
 
 }
 
+/*************************************************************************
+    Nombre de la función: 	CloseGprsConnection()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función para cerrar la conexión GPRS
+
+    Precondiciones:
+        Debe haber una conexión abierta
+
+    Returns
+        0 -> Se ha cerrado la conexión
+        1 -> Error en el establecimiento de conexión
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char CloseGprsConnection (void)
 {
     char *bufferToSend;
@@ -189,6 +398,25 @@ unsigned char CloseGprsConnection (void)
 
 }
 
+/*************************************************************************
+    Nombre de la función: 	GprsStartSequence()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Función con la secuencia de llamadas para establecer conectividad GPRS
+        Si se utiliza esta función no se necesita usar las anteriores de manera 
+        individual
+
+    Precondiciones:
+        Debe ejectarse un CheckSIM();
+
+    Returns
+        0 -> Se ha establecido conexión
+        1 -> Error en el establecimiento de conexión
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char GprsStartSequence (void)
 {
     char bufferToSend[50];
@@ -230,6 +458,24 @@ unsigned char GprsStartSequence (void)
     return 0;  
 }
 
+/*************************************************************************
+    Nombre de la función: 	GprsReconnectSequence()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Secuencia para reestablecer conectividad GPRS (esta función 
+        no abre socket).
+
+    Precondiciones:
+        Debe ejectarse un CheckSIM();
+
+    Returns
+        0 -> Se ha establecido conexión
+        1 -> Error en el establecimiento de conexión
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char GprsReconnectSequence (void)
 {
     char bufferToSend[50];
@@ -259,6 +505,23 @@ unsigned char GprsReconnectSequence (void)
     return 0;  
 }
 
+/*************************************************************************
+    Nombre de la función: 	GprsCloseSequence()
+    Responsable: 			Javier Alcina
+
+    Descripción:
+        Secuencia de cierre de conexión
+
+    Precondiciones:
+        Debe haber una conexión activa
+
+    Returns
+        0 -> Se ha establecido conexión
+        1 -> Error en el establecimiento de conexión
+  
+    Parametros
+        Ninguno
+************************************************************************/
 unsigned char GprsCloseSequence (void)
 {    
     if(CloseGprsSession ())
